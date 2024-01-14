36360d6de42654a894b880379c0dd21991c13eb9
Tornike-max <84152983+Tornike-max@users.noreply.github.com>
2024-01-14 18:48:15 +0400

    hm

---
+++ dev/posts/serializers.py
@@ -0,0 +1,25 @@
+from rest_framework import serializers
+from django.contrib.auth.models import User
+from .models import Post
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = [
+            'id',
+            'username',
+            'email'
+        ]
+
+class PostSerializer(serializers.ModelSerializer):
+    user = UserSerializer(read_only = True)
+    
+    class Meta:
+        model = Post
+        fields = [
+            'id',
+            'title',
+            'body',
+            'user',
+            'slug',
+        ]
---
+++ dev/posts/views.py
@@ -0,0 +1,38 @@
+from django.contrib.auth import authenticate, login, logout
+from rest_framework import generics, status
+from rest_framework.response import Response
+from rest_framework.views import APIView  
+from rest_framework.permissions import IsAuthenticated
+from .serializers import UserSerializer, PostSerializer
+from .models import Post 
+
+class RegisterView(generics.GenericAPIView):
+    serializer_class = UserSerializer
+
+class LoginView(APIView): 
+    def post(self, request):
+        username = request.data.get('username')
+        password = request.data.get('password')
+
+        user = authenticate(username=username, password=password)
+
+        if user:
+            login(request, user)
+            return Response(UserSerializer(user).data)
+
+        return Response({'error': 'Wrong Credentials'}, status=status.HTTP_400_BAD_REQUEST)
+
+class LogoutView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        logout(request)
+        return Response(status=status.HTTP_200_OK)
+
+class PostList(generics.ListCreateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
